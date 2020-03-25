52d9907408e68c8fabd15db6dd7c09de09e6772d
dronperminov <dronperminov@gmail.com>
2020-03-26 01:17:54 +0300

    fix mandelbulb formatting

--- Primitives/Mandelbulb.hpp
+++ Primitives/Mandelbulb.hpp
@@ -11,35 +11,36 @@ class Mandelbulb : public RayMarching {
 
 public:
     Mandelbulb(std::istream &is, Material material); // конструктор из потока
-
-    double DE(const Vec &pos) {
-        Vec z0 = pos - center;
-        Vec z = z0;
-        double dr = 1.0;
-        double r = 0.0;
-
-        for (int i = 0; i < 64; i++) {
-            r = z.Norm();
-            
-            if (r > radius)
-                break;
-            
-            double theta = acos(z.z / r);
-            double phi = atan2(z.y, z.x);
-            dr = pow(r, power - 1.0) * power * dr + 1.0;
-            
-            double zr = pow(r, power);
-            theta = theta * power;
-            phi = phi * power;
-            
-            z = Vec(sin(theta) * cos(phi), sin(phi) * sin(theta), cos(theta)) * zr + z0;
-        }
-
-        return 0.5*log(r)*r/dr;
-    }
+    double DE(const Vec &pos);
 };
 
 // конструктор из потока
 Mandelbulb::Mandelbulb(std::istream &is, Material material) : RayMarching(material) {
     is >> center >> power >> radius;
+}
+
+double Mandelbulb::DE(const Vec &pos) {
+    Vec z0 = pos - center;
+    Vec z = z0;
+    double dr = 1.0;
+    double r = 0.0;
+
+    for (int i = 0; i < 64; i++) {
+        r = z.Norm();
+        
+        if (r > radius)
+            break;
+        
+        double theta = acos(z.z / r);
+        double phi = atan2(z.y, z.x);
+        dr = pow(r, power - 1.0) * power * dr + 1.0;
+        
+        double zr = pow(r, power);
+        theta = theta * power;
+        phi = phi * power;
+        
+        z = Vec(sin(theta) * cos(phi), sin(phi) * sin(theta), cos(theta)) * zr + z0;
+    }
+
+    return 0.5*log(r)*r/dr;
 }
